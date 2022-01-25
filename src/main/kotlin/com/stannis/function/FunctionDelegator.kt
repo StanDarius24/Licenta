4ce395a251dfec130e2a474c6a497f7faf314194
StanDarius24 <standarius24@gmail.com>
2022-01-25 03:46:55 +0200

    method implementation bug todo

--- src/main/kotlin/com/stannis/function/FunctionDelegator.kt
+++ src/main/kotlin/com/stannis/function/FunctionDelegator.kt
@@ -2,6 +2,7 @@ package com.stannis.function
 
 import com.stannis.dataModel.statementTypes.FunctionDeclarator
 import com.stannis.dataModel.statementTypes.Name
+import com.stannis.dataModel.statementTypes.ParameterDeclaration
 import com.stannis.dataModel.statementTypes.QualifiedName
 
 object FunctionDelegator {
@@ -12,35 +13,81 @@ object FunctionDelegator {
             list = ArrayList()
             list!!.add(functionDeclarator)
         } else {
-            sw = true
+            sw = false
             list!!.forEach { data -> run {
-                if(data.parameter != null && functionDeclarator.parameter != null && (data.parameter!!.size == functionDeclarator.parameter!!.size))
-                if(functionDeclarator.name is QualifiedName) {
-                sw = sw xor solveQualifiedName(data, (functionDeclarator.name as QualifiedName))
-                } else if(functionDeclarator.name is Name) {
-                    sw = sw xor solveName(data, (functionDeclarator.name as Name))
-                }
+                    if (functionDeclarator.name is QualifiedName) {
+                        sw = sw xor solveQualifiedName(data, (functionDeclarator.name as QualifiedName), functionDeclarator)
+                    } else if (functionDeclarator.name is Name) {
+                        sw = sw xor solveName(data, (functionDeclarator.name as Name), functionDeclarator)
+                    }
             } }
         }
         if(!sw) {
             list!!.add(functionDeclarator)
+            println()
         }
     }
-    private fun solveName(functionDeclarator: FunctionDeclarator, name: Name): Boolean {
+    private fun solveName(
+        functionDeclarator: FunctionDeclarator,
+        name: Name,
+        functionDeclarator1: FunctionDeclarator
+    ): Boolean {
         return if(functionDeclarator.name is Name) {
-            name.equals(functionDeclarator.name)
+            checkParameters(functionDeclarator, name, functionDeclarator1)
         } else {
             false
         }
     }
 
-    private fun solveQualifiedName(function: FunctionDeclarator, qualifiedName: QualifiedName): Boolean {
-        return if(function.name is QualifiedName) {
-            val test1 = qualifiedName.lastName!!.equals((function.name as QualifiedName).lastName)
-            val test2 =(qualifiedName.qualifier?.get(0) as Name).name.equals(((function.name as QualifiedName).qualifier?.get(0) as Name).name)
-            test1 && test2
+    private fun checkParameters(
+        functionDeclarator: FunctionDeclarator,
+        name: Name,
+        functionDeclarator1: FunctionDeclarator
+    ): Boolean {
+        return if (name == functionDeclarator.name && functionDeclarator.parameter != null) {
+            var switch1 = 0
+            if (functionDeclarator1.parameter != null) {
+                functionDeclarator.parameter!!.iterator().forEachRemaining { param ->
+                    run {
+                        functionDeclarator1.parameter!!.iterator().forEachRemaining { param1 ->
+                            run {
+                                if ((param1 as ParameterDeclaration).declarator == (param as ParameterDeclaration).declarator) {
+                                    switch1 += 1
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            switch1 != functionDeclarator1.parameter!!.size
+        } else if(functionDeclarator.name is QualifiedName) {
+            return checkParameters(functionDeclarator1, (functionDeclarator.name as QualifiedName).lastName as Name, functionDeclarator)
         } else {
-            false
+            name == functionDeclarator.name
+        }
+    }
+
+    private fun solveQualifiedName(
+        function: FunctionDeclarator,
+        qualifiedName: QualifiedName,
+        functionDeclarator: FunctionDeclarator
+    ): Boolean {
+        return when (function.name) {
+            is QualifiedName -> {
+                val test1 = qualifiedName.lastName!! == (function.name as QualifiedName).lastName
+                val test2 =(qualifiedName.qualifier?.get(0) as Name).name.equals(((function.name as QualifiedName).qualifier?.get(0) as Name).name)
+                test1 && test2
+            }
+            is Name -> {
+                if(!checkParameters(functionDeclarator, function.name as Name, function)) {
+                    qualifiedName.lastName!! == function.name
+                } else {
+                    true
+                }
+            }
+            else -> {
+                false
+            }
         }
     }
 
